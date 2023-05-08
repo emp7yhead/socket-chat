6a536c8a57fe1692129766a7904d682fcddb83b6
emp7yhead <artyomkropp@gmail.com>
2023-05-09 03:04:20 +0900

    update logging level

--- socket_chat/scripts/server.py
+++ socket_chat/scripts/server.py
@@ -6,7 +6,7 @@ import sys
 from socket_chat.server import ChatServer
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 HOST: str = 'localhost'
 PORT: int = 5000

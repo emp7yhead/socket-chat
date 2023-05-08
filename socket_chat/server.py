422dd4ca3d3fdca58a7d5cec98d53b6e7b05a3c0
emp7yhead <artyomkropp@gmail.com>
2023-05-09 03:03:59 +0900

    decompose server

--- socket_chat/server.py
+++ socket_chat/server.py
@@ -8,28 +8,11 @@ class ChatServer:
         self._connections = set()
         self._host = host
         self._port = port
+        self.handler = ClientHandler
 
     async def handle_client(self, reader, writer):
-        conn = (reader, writer)
-        writer.write(b'welcome to chat!')
-        self._connections.add(conn)
-        await self._broadcast_data(b'New connection', writer)
-        logging.info(f'{conn} connected')
-        try:
-            while True:
-                data = await reader.read(1024)
-                if not data:
-                    break
-                await self._broadcast_data(data, writer)
-        finally:
-            writer.close()
-            self._connections.remove(conn)
-
-    async def _broadcast_data(self, data, sender_writer):
-        for _, c_writer in self._connections:
-            if c_writer != sender_writer:
-                c_writer.write(data)
-                await c_writer.drain()
+        handler = self.handler(reader, writer, self._connections)
+        await handler.handle()
 
     async def run(self):
         server = await asyncio.start_server(
@@ -40,3 +23,37 @@ class ChatServer:
         logging.info(f'Start server on {self._host}:{self._port}')
         async with server:
             await server.serve_forever()
+
+
+class ClientHandler:
+    def __init__(self, reader, writer, connections):
+        self._reader = reader
+        self._writer = writer
+        self._connections = connections
+
+    @property
+    def address(self):
+        return self._writer.get_extra_info('peername')
+
+    async def handle(self):
+        conn = (self._reader, self._writer)
+        self._writer.write(b'welcome to chat!')
+        self._connections.add(conn)
+        await self._broadcast_data(b'New connection')
+        logging.info(f'{self.address} connected')
+        try:
+            while True:
+                data = await self._reader.read(1024)
+                logging.debug(f'{self.address} send {data}')
+                if not data:
+                    break
+                await self._broadcast_data(data)
+        finally:
+            self._writer.close()
+            self._connections.remove(conn)
+
+    async def _broadcast_data(self, data):
+        for _, c_writer in self._connections:
+            if c_writer != self._writer:
+                c_writer.write(data)
+                await c_writer.drain()

b5a52a8f85110d35003d02773e24437d68c74dd3
emp7yhead <artyomkropp@gmail.com>
2023-05-09 02:47:13 +0900

    update client to async

--- socket_chat/client.py
+++ socket_chat/client.py
@@ -1,13 +1,29 @@
-import logging
+import asyncio
+import aioconsole
 
-logger = logging.getLogger(__name__)
 
+class ChatClient:
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
 
-def is_server(addr, socket):
-    try:
-        socket.connect(addr)
-        logger.info('Connection established')
-    except OSError:
-        logger.info('Server not available')
-        return False
-    return True
+    async def _read_data(self, reader):
+        while True:
+            data = await reader.read(1024)
+            if not data:
+                break
+            print(f'Recieved: {data.decode()}')
+
+    async def _send_messages(self, writer):
+        while True:
+            message = await aioconsole.ainput()
+            writer.write(message.encode())
+            await writer.drain()
+
+    async def start(self):
+        reader, writer = await asyncio.open_connection(self._host, self._port)
+        try:
+            asyncio.create_task(self._read_data(reader))
+            await self._send_messages(writer)
+        finally:
+            writer.close()
--- socket_chat/scripts/client.py
+++ socket_chat/scripts/client.py
@@ -1,19 +1,23 @@
-import prompt
-import socket
+#! /usr/bin/env python
+import asyncio
+import sys
 import logging
+from socket_chat.client import ChatClient
 
-from socket_chat.client import is_server
+HOST: str = 'localhost'
+PORT: int = 5000
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-ADDRESS = ('localhost', 5000)
+
+def main() -> None:
+    client = ChatClient(HOST, PORT)
+    try:
+        asyncio.run(client.start())
+    except KeyboardInterrupt:
+        sys.exit(1)
 
 
-def run() -> None:
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    if is_server(ADDRESS, client_socket):
-        while True:
-            request = prompt.string('> ')
-            client_socket.sendall(request.encode())
+if __name__ == '__main__':
+    main()

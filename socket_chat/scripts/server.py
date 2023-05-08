9b7fb646dbdadd2f957e4785fa8d38dfc08caf47
emp7yhead <artyomkropp@gmail.com>
2023-05-09 02:47:23 +0900

    improve server

--- socket_chat/scripts/server.py
+++ socket_chat/scripts/server.py
@@ -1,33 +1,24 @@
-import socket
+#! /usr/bin/env python
+import asyncio
 import logging
+import sys
 
-from socket_chat.server import decode_request
+from socket_chat.server import ChatServer
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-ADDRESS = ('localhost', 5000)
+HOST: str = 'localhost'
+PORT: int = 5000
 
 
-def run() -> None:
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(ADDRESS)
-    server_socket.listen()
-    logger.info('init server socket')
-
-    while True:
-        client_socket, addr = server_socket.accept()
-        logger.info(f'Get connection from {addr}')
-        while True:
-            request = client_socket.recv(4096)
-            logger.info(request)
-            if not request:
-                break
-            msg = decode_request(request)
-            client_socket.send(f'{addr[0]} send message: {msg}\n'.encode())
-        client_socket.close()
+def main() -> None:
+    chat = ChatServer(HOST, PORT)
+    try:
+        asyncio.run(chat.run())
+    except KeyboardInterrupt:
+        sys.exit(1)
 
 
 if __name__ == '__main__':
-    run()
+    main()
--- socket_chat/server.py
+++ socket_chat/server.py
@@ -1,3 +1,42 @@
-def decode_request(request: bytes) -> str:
-    decoded_request = request.decode()
-    return decoded_request.rstrip('\n')
+import asyncio
+import logging
+
+
+class ChatServer:
+
+    def __init__(self, host, port):
+        self._connections = set()
+        self._host = host
+        self._port = port
+
+    async def handle_client(self, reader, writer):
+        conn = (reader, writer)
+        writer.write(b'welcome to chat!')
+        self._connections.add(conn)
+        await self._broadcast_data(b'New connection', writer)
+        logging.info(f'{conn} connected')
+        try:
+            while True:
+                data = await reader.read(1024)
+                if not data:
+                    break
+                await self._broadcast_data(data, writer)
+        finally:
+            writer.close()
+            self._connections.remove(conn)
+
+    async def _broadcast_data(self, data, sender_writer):
+        for _, c_writer in self._connections:
+            if c_writer != sender_writer:
+                c_writer.write(data)
+                await c_writer.drain()
+
+    async def run(self):
+        server = await asyncio.start_server(
+            self.handle_client,
+            self._host,
+            self._port
+        )
+        logging.info(f'Start server on {self._host}:{self._port}')
+        async with server:
+            await server.serve_forever()
